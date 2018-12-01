2b244d9258347bfbfc1dedc3c80f0fc494e4ea58
Krzysztof Lewko <krzys.minib00m@gmail.com>
2018-12-01 09:36:15 +0100

    Complete solution for day1 part 1

--- day1/solution.asm
+++ day1/solution.asm
@@ -3,28 +3,40 @@ global main
 extern scanf
 extern printf
 
+EOF equ -1
+
 section .data
-integer_format: db "%d", 0
-integer_format2: db "%d", 10, 0
+input_int64_format: db "%ld", 0
+output_int64_format: db "%ld", 10, 0
 
 section .text
 
 main:
     push    rbp
     mov     rbp, rsp
-    sub     rsp, 16 ; int and stack alignment
+    sub     rsp, 32 ; int64 for input, int64 for result, stack alignment
+    mov     qword [rbp - 16], 0
 
-    mov     rdi, integer_format
+.read_more:
+    mov     rdi, input_int64_format
     lea     rsi, [rbp - 8]
     mov     al, 0
     call    scanf
 
-    mov     rsi, qword [rbp - 8]
-    mov     rdi, integer_format2
+    cmp     eax, EOF
+    je      .print_result
+
+    mov     r8, qword [rbp - 8]
+    add     qword [rbp - 16], r8
+    jmp     .read_more
+
+.print_result:
+    mov     rsi, qword [rbp - 16]
+    mov     rdi, output_int64_format
     mov     al, 0
     call    printf
 
-    add     rsp, 16
+    add     rsp, 32
     pop     rbp
     mov     rax, 0
     ret
