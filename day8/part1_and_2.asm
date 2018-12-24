ddc70fcdd48fb0c293a0779401bcdf5a88cc8989
Krzysztof Lewko <krzysztof.lewko@beacon.io>
2018-12-24 11:13:50 +0100

    Add solution to day8 part2; merge part1 and part2 into one file

--- day8/part1_and_2.asm
+++ day8/part1_and_2.asm
@@ -2,6 +2,8 @@ global main
 
 extern scanf
 extern printf
+extern calloc
+extern free
 
 section .data
 INT64_INPUT: db "%ld", 0
@@ -12,11 +14,14 @@ calculate_licence_sum:
     push        rbp
     push        r12 ; sum of childs entries
     push        r13 ; current child
+    push        r14 ; ptr to buffer
+    push        r15 ; sum of childs, according to metadata
     mov         rbp, rsp
-    sub         rsp, 16 ; number of child nodes, number of metadata entries
+    sub         rsp, 32 ; number of metadata entries, number of child nodes, metadata entry
 
     xor         r12, r12
     xor         r13, r13
+    xor         r15, r15
 
     mov         rdi, INT64_INPUT
     lea         rsi, [rbp - 16]
@@ -28,12 +33,19 @@ calculate_licence_sum:
     mov         eax, 0
     call        scanf
 
+    mov         rdi, [rbp - 16]
+    inc         rdi ; 1 based indexing
+    mov         rsi, 8
+    call        calloc
+    mov         r14, rax
+
     .read_next_child_entry:
     cmp         r13, qword [rbp - 16]
     je          .read_self_meta_entries
     call        calculate_licence_sum
     add         r12, rax
     inc         r13
+    mov         [r14 + r13 * 8], rcx
     jmp         .read_next_child_entry
 
     .read_self_meta_entries:
@@ -42,16 +54,32 @@ calculate_licence_sum:
     cmp         r13, qword [rbp - 8]
     je          .return_result
     mov         rdi, INT64_INPUT
-    lea         rsi, [rbp - 16] ; hijacking this memory
+    lea         rsi, [rbp - 24]
     mov         eax, 0
     call        scanf
-    add         r12, qword [rbp - 16]
+    add         r12, qword [rbp - 24]
     inc         r13
+
+    cmp         qword [rbp - 16], 0 ; if its zero, sum all meta-entries, otherwise sum according to metadata
+    je          .read_next_meta_entry
+    mov         rax, [rbp - 24]
+    cmp         rax, 0
+    je          .read_next_meta_entry
+    cmp         rax, [rbp - 16]
+    jg          .read_next_meta_entry
+    add         r15, [r14 + rax * 8]
     jmp         .read_next_meta_entry
 
     .return_result:
+    cmp         qword [rbp - 16], 0
+    cmove       r15, r12
+    mov         rdi, r14
+    call        free
     mov         rax, r12
-    add         rsp, 16
+    mov         rcx, r15
+    add         rsp, 32
+    pop         r15
+    pop         r14
     pop         r13
     pop         r12
     pop         rbp
@@ -60,12 +88,22 @@ calculate_licence_sum:
 
 
 main:
-    sub         rsp, 8
+    push        rbp
+    mov         rbp, rsp
+    sub         rsp, 16
+
     call        calculate_licence_sum
+    mov         qword [rbp - 16], rcx
     mov         rdi, INT64_OUTPUT
     mov         rsi, rax
     mov         eax, 0
     call        printf
 
-    add         rsp, 8
+    mov         rdi, INT64_INPUT
+    mov         rsi, qword [rbp - 16]
+    mov         eax, 0
+    call        printf
+
+    add         rsp, 16
+    pop         rbp
     ret
